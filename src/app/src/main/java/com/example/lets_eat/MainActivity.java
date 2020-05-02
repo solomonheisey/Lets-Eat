01bc93fc322db2a2525d251c2ea4d3b65d78179b
solomonheisey <solheisey25@gmail.com>
2020-05-02 19:17:04 -0400

    Added a let's eat button that currently does nothing

--- src/app/src/main/java/com/example/lets_eat/MainActivity.java
+++ src/app/src/main/java/com/example/lets_eat/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.lets_eat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "InitialLetsEat";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btnLetsEat);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Let's Eat button has been pressed");
+                Toast.makeText(getApplicationContext(), "Recipe Generated!", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
     }
+
+
+
 }
