f8d5761c8ca7155fa119207df2efdc406f160606
solomonheisey <solheisey25@gmail.com>
2020-05-03 11:57:54 -0400

    app can generate names!

--- src/app/src/main/java/com/example/lets_eat/RecipeView.java
+++ src/app/src/main/java/com/example/lets_eat/RecipeView.java
@@ -5,8 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class RecipeView extends AppCompatActivity {
+    private TextView jsonRecipe;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +30,38 @@ public class RecipeView extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_recipe_view);
+
+        jsonRecipe = findViewById(R.id.jsonRecipe);
+
+        mQueue = Volley.newRequestQueue(this);
+        jsonParse();
+    }
+
+     private void jsonParse() {
+        String url = "https://www.themealdb.com/api/json/v1/1/random.php";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("meals");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject recipe = jsonArray.getJSONObject(i);
+
+                                String strMeal = recipe.getString("strMeal");
+                                jsonRecipe.append(strMeal);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
     }
 }
