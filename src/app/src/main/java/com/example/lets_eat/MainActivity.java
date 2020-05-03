9c2ab94373c38e11608fc9b195cd0004ba8999e7
solomonheisey <solheisey25@gmail.com>
2020-05-02 20:32:11 -0400

    added background

--- src/app/src/main/java/com/example/lets_eat/MainActivity.java
+++ src/app/src/main/java/com/example/lets_eat/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.lets_eat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -14,9 +17,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         Button btn = findViewById(R.id.btnLetsEat);
+        btn.setTextColor(Color.WHITE);
+        btn.setBackgroundColor(0x7933ff);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
 
 
 
